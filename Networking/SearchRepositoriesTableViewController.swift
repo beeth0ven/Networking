d049d5c11392d14a3d4937224c442d03602934b9
beeth0ven <beeth0vendev@163.com>
2016-08-25 23:44:08 +0800

    Scan

--- Networking/SearchRepositoriesTableViewController.swift
+++ Networking/SearchRepositoriesTableViewController.swift
@@ -15,12 +15,22 @@ class SearchRepositoriesTableViewController: UITableViewController {
     @IBOutlet private weak var searchBar: UISearchBar!
     
     let disposeBag = DisposeBag()
-
+    
+    let rx_repositories = Scan(seed: [Repository]())
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = nil
         tableView.delegate = nil
+        
+        rx_repositories
+            .asObservable()
+            .bindTo(tableView.rx_itemsWithCellIdentifier("UITableViewCell")) { row, repository, cell in
+                cell.textLabel?.text = repository.name
+                cell.detailTextLabel?.text = repository.description
+            }
+            .addDisposableTo(disposeBag)
         
         searchBar.rx_text
             .filter { text in !text.isEmpty }
@@ -28,10 +38,45 @@ class SearchRepositoriesTableViewController: UITableViewController {
             .distinctUntilChanged()
             .flatMapLatest { text in Github.searchRepositories(text: text) }
             .doOnError { error in print(error) }
-            .bindTo(tableView.rx_itemsWithCellIdentifier("UITableViewCell")) { row, repository, cell in
-                cell.textLabel?.text = repository.name
-                cell.detailTextLabel?.text = repository.description
+            .subscribeNext { [unowned self] (repositories) in
+                self.rx_repositories.updateElement.value = { items in
+                    items += repositories
+                }
             }
             .addDisposableTo(disposeBag)
+        
+        tableView.rx_itemDeleted
+            .subscribeNext { [unowned self] indexPath in
+                self.rx_repositories.updateElement.value = { items in
+                    items.removeAtIndex(indexPath.row)
+                }
+            }
+            .addDisposableTo(disposeBag)
+    }
+    
+    @IBAction func doAdd(sender: UIBarButtonItem) {
+        rx_repositories.updateElement.value = { items in
+            let r = Repository(name: "luojie", language: "luojie", description: "luojie", url: "luojie")
+            items.insert(r, atIndex: 0)
+        }
+    }
+    
+}
+
+struct Scan<Element> {
+    
+    let updateElement: Variable<((inout Element) -> Void)> = Variable ({ _ in })
+    
+    var seed: Element
+    
+    func asObservable() -> Observable<Element> {
+        return updateElement
+            .asObservable()
+            .scan(seed) { (element, updateElement) -> Element in
+                var element = element
+                updateElement(&element);
+                return element
+        }
+        
     }
 }
